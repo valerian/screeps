12fef2addfea0a1da01d26823b00968267a85d10
Valerian Gaudeau <valerian.gaudeau@gmail.com>
2016-07-05 20:46:04 +0200

    improved log dumping to avoid creating instances of LogEntry for nothing

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -4,30 +4,24 @@ import { Config } from '../config/config'
 export namespace Console {
     
     export function init() {
-        global['dumpLogs'] = dumpLogs;
+        global['logdump'] = logdump;
         global['warnMode'] = warnMode;
         global['infoMode'] = infoMode;
         global['debugMode'] = debugMode;
         global['traceMode'] = traceMode;
     }
 
-    export function dumpLogs(level: Log.LEVEL = Log.LEVEL.INFO): boolean {
+    export function logdump(level: Log.LEVEL = Log.LEVEL.INFO): boolean {
         let version = level >= Log.LEVEL.INFO ? 'logInfo' : 'log';
         let counter: number = 0;
-        if (!Memory[version])
-            return false;
         console.log('[LOG DUMP STARTING]');
-        for (let i in Memory[version]) {
-            if (Memory[version][i]['level'] >= level) {
-                let line = new Log.LogEntry(Memory[version][i].level,
-                                            Memory[version][i].message,
-                                            Memory[version][i].context,
-                                            Memory[version][i].date,
-                                            Memory[version][i].tick);
-                console.log('[' + i + '] ' + line.getLine());
-                counter++;
+        if (Memory[version])
+            for (let i in Memory[version]) {
+                if (Memory[version][i]['level'] >= level) {
+                    console.log('[' + i + '] ' + Log.LogEntry.prototype.getLine.apply(Memory[version][i]));
+                    counter++;
+                }
             }
-        }
         console.log('[LOG DUMP ENDED] [' + counter + ' ENTRIES DUMPED]');
         return true;
     }
